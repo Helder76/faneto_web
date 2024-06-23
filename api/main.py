d162428adaede3262a3afc6bd8e2e65be8a18522
Helder Freitas <freitas.h@gmail.com>
2024-06-22 22:17:24 -0300

    criando templates

--- api/main.py
+++ api/main.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('home.html')
+
+@app.route("/clientes")
+def clientes():
+    return render_template('clientes.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
